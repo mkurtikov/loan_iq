0dc2acebd7626332849e02dfbc7887917f91375d
ilyadevops <40355728+ilyadevops@users.noreply.github.com>
2019-06-06 16:18:39 +0300

    Delete script.js

--- roles/MONGO/files/script.js
+++
@@ -1,5 +0,0 @@
-db.getCollection('documents')
-.find({"url": /fblqa.develop.fcbt/})
-.forEach(function(e,i) {e.url=e.url.replace("fblqa.develop.fcbt", "fblenkedevbe.develop.fcbt");
-db.getCollection('documents').save(e);
-});
